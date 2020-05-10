fcbad3f478f6aef13de95ce591d374787db82688
Lidia Opuchlik <s16478@pja.edu.pl>
2020-05-10 09:38:20 +0200

    Zadanie 6.1-sprawdzanie numeru indeksu (autoryzacja)

--- aplikacja5/Startup.cs
+++ aplikacja5/Startup.cs
@@ -1,6 +1,7 @@
 using aplikacja5.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,12 +26,33 @@ namespace aplikacja5
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IStudentsDbService service)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.Use(async (context, next) =>
+            {
+                if (!context.Request.Headers.ContainsKey("Index"))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("You have to give your index number!");
+                    return;
+                }
+
+                string index = context.Request.Headers["Index"].ToString();
+                var student = service.GetStudent(index);
+                if (student == null)
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("This index number does not exist!");
+                    return;
+                }
+
+                await next();
+            });
 
             app.UseRouting();
 
